b8c92c2bbfb4364c14bc124b47b27b43bac6c33d
Sai Arvind <sai.arvindg@gmail.com>
2018-01-24 21:56:34 -0500

    working extracted quotes

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 visit this link for datascraper: https://github.com/Derpthemeus/LeagueOfQuotes
 
 this link to conver images to ascii: http://picascii.com
+or use this: https://github.com/IonicaBizau/image-to-ascii
+or use this: https://blog.filestack.com/api/an-api-to-convert-any-image-into-ascii/
+
 
 links to pictures:
--- quote_extractor.py
+++ quote_extractor.py
@@ -1,7 +1,8 @@
 import json
+import re
 
 orig_json_data = open("original_data.json").read()
-#orig_json_data.replace('""','"')
+extracted_data_file = open("extracted_data.txt","w")
 
 orig_data = json.loads(orig_json_data)
 
@@ -20,15 +21,31 @@ for champ in champ_list:
 	n = champ['name']
 	reduced_champ_list[i] = n
 
-# for c in reduced_champ_list:
-# 	print("Champ data: " + c + " -> " + reduced_champ_list[c])
 
 for i in quiz_data:
 	champ = reduced_champ_list[i]
 	champ_id_dict = quiz_data[i] # will get the quotes and triggers so {quotes: {...}, triggers:[...]
-	quotes_dict = champ_id_dict['quotes'] # will get just the quotes dict so {champname.attack: {...}}
-	quote_text = quotes_dict['text']
-	print(quote_text)
+	quotes_dict = champ_id_dict['quotes'] # will get just the quotes dict so {champname.action: {...}}
+	for q in quotes_dict: # q will be the champname.action
+		actual_quote = quotes_dict[q]['text']
+		champ_quotes[actual_quote] = champ
 
-print(champ_quotes)
+for x in champ_quotes:
+	print(x + " - " + champ_quotes[x])
+	cq = x + " - " + champ_quotes[x]
+
+	# take out weird unicode characters and keep ascii only
+	cq_clean = re.sub(r'[^\x00-\x7f]',r'',cq)
+
+	# replacing new lines with tabs
+	# Reasoning: in the actual motd, a random line number will be chosen and that will be printed out
+	# So if there is quote that has new lines in it, then the rest of the quote will not be printed
+	# If there are tabs, then a simple regex can be performed to replace tabs with new line right after
+	# the line is selected
+	cq_clean = re.sub(r'\n',r'\t',cq_clean)
+
+	extracted_data_file.write(cq_clean)
+	extracted_data_file.write("\n") # so a line number can be chosen properly
+
+extracted_data_file.close()
 
