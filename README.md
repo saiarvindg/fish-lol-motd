e1724937a8e31e34af3f2877abf14fae4c14e9e6
Sai Arvind <sai.arvindg@gmail.com>
2018-01-24 20:19:59 -0500

    added link to image to ascii generator

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Contains champions up to zoe
 
 visit this link for datascraper: https://github.com/Derpthemeus/LeagueOfQuotes
+
+this link to conver images to ascii: http://picascii.com
+
+links to pictures:
--- quote_extractor.py
+++ quote_extractor.py
@@ -1,14 +1,34 @@
 import json
 
 orig_json_data = open("original_data.json").read()
+#orig_json_data.replace('""','"')
 
 orig_data = json.loads(orig_json_data)
 
-t = type(orig_data["quizData"])
-q = orig_data["quizData"]
-qkeys = q.keys()
 
-for k in qkeys:
-	print(k)
+# get rid of the icon information - what we need the is the id and name
+# like this: {1:"Annie",2:"Aatrox"}
+reduced_champ_list = {}
+champ_quotes = {}
 
-#print(p)
+quiz_data = orig_data['quizData']
+champ_list = orig_data['championList']
+
+# add elements to reduced champ list
+for champ in champ_list:
+	i = champ['id']
+	n = champ['name']
+	reduced_champ_list[i] = n
+
+# for c in reduced_champ_list:
+# 	print("Champ data: " + c + " -> " + reduced_champ_list[c])
+
+for i in quiz_data:
+	champ = reduced_champ_list[i]
+	champ_id_dict = quiz_data[i] # will get the quotes and triggers so {quotes: {...}, triggers:[...]
+	quotes_dict = champ_id_dict['quotes'] # will get just the quotes dict so {champname.attack: {...}}
+	quote_text = quotes_dict['text']
+	print(quote_text)
+
+print(champ_quotes)
+
